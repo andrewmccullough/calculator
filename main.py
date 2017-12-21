cc50dfece58385e41b92e3c3f4ad6f095705b21f
Andrew McCullough <andrewmccullough@users.noreply.github.com>
2017-12-20 21:45:34 -0500

    Instructions, "Goodbye.", better error handling.
    Separates message rendering into its own function.
    
    Script now says "Goodbye." after last interaction.
    
    Warns of parentheses.
    
    Catches double operators.

--- main.py
+++ main.py
@@ -1,102 +1,127 @@
-import sys
+import sys, re, os
 
-### constants
+### constants ###
 
 operators = ["/", "*", "+", "-"]
 
+def render(message, after = True):
+    if after:
+        print(message)
+        print("~" * len(message))
+    else:
+        print("~" * len(message))
+        print(message)
+
 def kill(message):
-    print("~" * len(message))
-    print(message)
+    os.system("clear")
+    render(message, False)
+    render("Goodbye.")
     sys.exit()
 
-### action
+### action ###
 
+os.system("clear")
+render("Press return on a new line to exit.")
 expression = input().strip()
-terms = expression.split()
-future = []
 
-for number in range(1, len(terms), 2):
+while expression != "":
 
-    if terms[number] not in operators:
-        kill("Every pair of terms should have an operator between them.")
+    if "(" in expression or ")" in expression:
+        kill("This script doesn't understand parentheses yet.")
 
-# First pass, multiplication and division
-i = 0
-while i < len(terms):
+    terms = expression.split()
+    future = []
 
-    term = terms[i]
+    for i in range(1, len(terms), 2):
 
-    if term in operators:
+        if terms[i] not in operators:
+            RegEx = re.compile(r"^[\*\-\+\/]+$")
+            if RegEx.match(terms[i]):
+                kill(terms[i] + " is not a valid operator.")
+            else:
+                kill("Every two terms should have an operator in between.")
+
+    # First pass, multiplication and division
+    i = 0
+    while i < len(terms):
+
+        term = terms[i]
+
+        if term in operators:
+
+            if term in ["+", "-"]:
+                future.append(terms[i - 1])
+                future.append(term)
+            else:
+                a = float(terms[i - 1])
+                b = float(terms[i + 1])
+
+                if term == "*":
+                    result = a * b
+                else:
+                    result = a / b
+
+                future.append(result)
+                terms = future + terms[i + 1 + 1:]
+                future = []
+                i = 0
+
+        else:
+            try:
+                float(term)
+            except ValueError:
+                kill(term + " is not a valid number.")
+
+        i = i + 1
+
+    future = []
+
+    # Second pass, addition and subtraction
+    i = 0
+    while i < len(terms):
+
+        term = terms[i]
 
         if term in ["+", "-"]:
-            future.append(terms[i - 1])
-            future.append(term)
-        else:
             a = float(terms[i - 1])
             b = float(terms[i + 1])
 
-            if term == "*":
-                result = a * b
+            if term == "+":
+                result = a + b
             else:
-                result = a / b
+                result = a - b
 
             future.append(result)
             terms = future + terms[i + 1 + 1:]
             future = []
             i = 0
 
+        i = i + 1
+
+    if len(terms) > 1:
+
+        t = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+
+        message = "[" + t + "]\n     [remaining terms] " + terms + "\n     [ user  input ] " + userinput + "\n"
+
+        f = open("errors.log", "a")
+        f.write(message)
+
+        kill("There's been a problem. Check the log for details.")
+
+    if terms[0] == int(terms[0]):
+        result = int(terms[0])
     else:
-        try:
-            float(term)
-        except ValueError:
-            kill(term + " is not a valid number.")
+        result = terms[0]
 
-    i = i + 1
+    message = "= " + str(result)
+    print(message)
 
-future = []
+    if len(message) > len(expression):
+        print("~" * len(message))
+    else:
+        print("~" * len(expression))
 
-# Second pass, addition and subtraction
-i = 0
-while i < len(terms):
+    expression = input().strip()
 
-    term = terms[i]
-
-    if term in ["+", "-"]:
-        a = float(terms[i - 1])
-        b = float(terms[i + 1])
-
-        if term == "+":
-            result = a + b
-        else:
-            result = a - b
-
-        future.append(result)
-        terms = future + terms[i + 1 + 1:]
-        future = []
-        i = 0
-
-    i = i + 1
-
-if len(terms) > 1:
-
-    t = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-
-    message = "[" + t + "]\n     [remaining terms] " + terms + "\n     [ user  input ] " + userinput + "\n"
-
-    f = open("errors.log", "a")
-    f.write(message)
-
-    kill("There's been a problem. Check the log for details.")
-
-if terms[0] == int(terms[0]):
-    result = int(terms[0])
-else:
-    result = terms[0]
-
-message = "= " + str(result)
-print(message)
-
-if len(message) > len(expression):
-    print("~" * len(message))
-else:
-    print("~" * len(expression))
+render("Goodbye.")
